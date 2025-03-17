5294e973e4ca4b045a567f90abdb3f6120e10e2a
Praveen593 <007praveen2003@gmail.com>
2025-03-17 18:32:21 +0530

    clerk issuse

--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -1,28 +1,42 @@
 import {SignedIn , SignedOut ,SignInButton , UserButton} from "@clerk/nextjs"
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./button";
+import { LayoutDashboard } from "lucide-react";
 export const Header = () => {
   return (
     <div className="fixed top-0 w-full bg-black/70 backdrop-blur-md z-50 border-b ">
-      <nav className="container mx-auto px-4 flex items-center justify-between">
+      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/">
         <Image
         src={"/logo.png"}
         alt="logo"
         width={150}
         height={200}
-        />
+        /> 
         </Link>
-      
+        <div>
+          <SignedIn>
+            <Link href={"/dashboard"}>
+            <Button variant="outline">
+              <LayoutDashboard size={18}/>
+              <span>Dashboard</span>
+            </Button>
+            </Link>
+          </SignedIn>
             <SignedOut>   
-              <SignInButton />
+              <SignInButton forceRedirectUrl="/dashboard">
+                <Button variant="outline"> Login </Button>
+              </SignInButton >
             </SignedOut>
             <SignedIn>
               <UserButton /> 
-            </SignedIn>  
-            </nav>
+            </SignedIn> 
+            </div> 
+            </nav>       
     </div>
      //intergrated the signup page using clerk
   );
 };
-export default Header ;
+export default Header ; //inital development of nav bar
+
