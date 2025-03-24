91027d54dc761d4b97ad9e02e4ea5b8879b67ef8
Praveen593 <007praveen2003@gmail.com>
2025-03-24 14:56:15 +0530

    nav revisit

--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -1,9 +1,13 @@
 import {SignedIn , SignedOut ,SignInButton , UserButton} from "@clerk/nextjs"
+import Link from "next/link";
 import Image from "next/image";
+import { Button } from "./button";
+import { PenBox ,LayoutDashboard } from "lucide-react"; 
+
 export const Header = () => {
   return (
     <div className="fixed top-0 w-full bg-black/70 backdrop-blur-md z-50 border-b ">
-      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+      <nav className="container mx-auto px-4 flex items-center justify-between">
         <Link href="/">
         <Image
         src={"/logo.png"}
@@ -12,17 +16,38 @@ export const Header = () => {
         height={200}
         />
         </Link>
-      
-            <SignedOut>   
-              <SignInButton />
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
+            <Button variant="outline">
+              <LayoutDashboard size={18}/>
+              <span className="hidden md:inline">Dashboard</span>
+            </Button>
+            </Link>
+
+            <Link href={"/transaction/create"} className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
+            <Button >
+              <PenBox size={18}/>
+              <span className="hidden md:inline">Add Transation</span>
+            </Button>
+            </Link>
+          </SignedIn>
+            <SignedOut> 
+            <SignInButton forceRedirectUrl="/dashboard"> 
+              <Button variant="outline">Login</Button>
+            </SignInButton>              
             </SignedOut>
             <SignedIn>
-              <UserButton /> 
-            </SignedIn>  
-            </nav>
-    </div>
+              <UserButton appearance={{
+                elements :{
+                  avatarBox :"w-10 h-10",
+                }
+              }}/> 
+            </SignedIn> 
+          </div> 
+      </nav>
+  </div>
      //intergrated the signup page using clerk
   );
 };
-export default Header ; //inital development of nav bar
-
+export default Header ;
