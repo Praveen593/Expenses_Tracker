1c8276cee12d1b3e9f696376875df42738cf801d
Praveen593 <007praveen2003@gmail.com>
2025-03-24 14:41:25 +0530

    nav progress

--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -1,6 +1,9 @@
 import {SignedIn , SignedOut ,SignInButton , UserButton} from "@clerk/nextjs"
 import Link from "next/link";
 import Image from "next/image";
+import { Button } from "./button";
+import { PenBox ,LayoutDashboard } from "lucide-react"; 
+
 export const Header = () => {
   return (
     <div className="fixed top-0 w-full bg-black/70 backdrop-blur-md z-50 border-b ">
@@ -13,15 +16,37 @@ export const Header = () => {
         height={200}
         />
         </Link>
-      
-            <SignedOut>   
-              <SignInButton />
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
+            <Button variant="outline">
+              <LayoutDashboard size={18}/>
+              <span className="hidden md:inline">Dashboard</span>
+            </Button>
+            </Link>
+
+            <Link href={"/transaction/create"} className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
+            <Button >
+              <PenBox size={18}/>
+              <span className="hidden md:inline">Add Transation</span>
+            </Button>
+            </Link>
+          </SignedIn>
+            <SignedOut> 
+            <SignInButton forceRedirectUrl="/dashboard"> 
+              <Button variant="outline">Login</Button>
+            </SignInButton>              
             </SignedOut>
             <SignedIn>
-              <UserButton /> 
-            </SignedIn>  
-            </nav>
-    </div>
+              <UserButton appearance={{
+                elements :{
+                  avatarBox :"text-2xl",
+                }
+              }}/> 
+            </SignedIn> 
+          </div> 
+      </nav>
+  </div>
      //intergrated the signup page using clerk
   );
 };
