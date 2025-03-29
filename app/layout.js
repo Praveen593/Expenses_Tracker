73d50724ee1164840657e485dcd2535ae131f9fb
Praveen593 <007praveen2003@gmail.com>
2025-03-29 22:38:52 +0530

    basic routes

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
     <ClerkProvider>  
     <html lang="en">
       <body
-        className={`${inter.className} bg-black`  } 
+        className={`${inter.className}`  } 
       >{/* header */}
       <Header/> 
       <main className="min-h-screen pt-14">
